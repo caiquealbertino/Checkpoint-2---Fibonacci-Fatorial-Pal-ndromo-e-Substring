5e2c37959b065c027839c962c361230382482586
caiquealbertino <caiqueealbertino@gmail.com>
2025-05-20 12:46:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
-//Kauê Silva Matheus - RM561675
-//Caique Baptistella de Vicente Albertino - RM564747
-//Arthur da Silva Alencar - RM563684
-//João Pedro de Moura Albino - RM565323
+//Kauê Silva Matheus - RM561675 (case 2)
+//Caique Baptistella de Vicente Albertino - RM564747 (case 1)
+//Arthur da Silva Alencar - RM563684 (case 4)
+//João Pedro de Moura Albino - RM565323 (case 3)
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+void calcularFibonacci(int numero);
+void calcularFatoriais(int limite, unsigned long long resultado[]);
+int verificar_palindromo(const char *palavra);
+int contemSubstring(const char *stringPrincipal, const char *substring);
 
 int main() {
     int opcao;
@@ -18,31 +23,21 @@ int main() {
 
     switch(opcao) {
         case 1: {
-            void calcularFibonacci(int numero)
-            unsigned long long fib[numero];
-            fib[0] = 0;
-            if (numero > 1) {
-                fib[1] = 1;
+            printf("\nVoce escolheu a opcao de Fibonacci\n");
+            int numero;
+            printf("Quantos termos da sequencia de Fibonacci deseja? ");
+            scanf("%d", &numero);
+            getchar();
+            
+            if (numero < 1) {
+                printf("Numero invalido! Digite um valor positivo.\n");
+                break;
             }
-
-            for (int i = 2; i < numero; i++) {
-                fib[i] = fib[i-1] + fib[i-2];
-            }
-
-            printf("Os %d primeiros termos da sequencia de Fibonacci: \n", numero);
-            for (int i = 0; i < numero; i++) {
-                printf("%llu ", fib[i]);
-            }
-            printf("\n");
+            
+            calcularFibonacci(numero);
             break;
         }
         case 2: {
-            void calcularFatoriais(int limite, unsigned long long resultado[]) {
-                resultado[0] = 1;
-                for (int i = 2; i <= limite; i++) {
-                    resultado[i - 1] = resultado[i - 2] * i;
-                }
-        }
             printf("\nVoce escolheu a opcao de Fatoriais\n");
             int numero;
             printf("\nEscolha um numero de 1 a 20: ");
@@ -51,7 +46,7 @@ int main() {
         
             if (numero < 1 || numero > 20) {
                 printf("Numero invalido! Digite um valor entre 1 e 20.\n");
-                return 1;
+                break;
             }
 
             unsigned long long fatoriais[numero];
@@ -63,8 +58,76 @@ int main() {
             }
             break;
         }
-  
         case 3: {
+            printf("Voce escolheu a opcao de verificar Palindromo\n");
+            char palavra[101];
+            int eh_palindromo;
+
+            printf("Digite uma palavra (sem espaços, ate 100 letras): ");
+            scanf("%100s", palavra); 
+            getchar();
+    
+            eh_palindromo = verificar_palindromo(palavra);
+    
+            if (eh_palindromo) {
+                printf("\"%s\" e um palindromo!\n", palavra);
+            } else {
+                printf("\"%s\" nao e um palindromo.\n", palavra);
+            }
+            break;
+        }
+        case 4: {
+            char stringPrincipal[100], substring[100];
+            
+            printf("Digite a string principal (max 99 caracteres): ");
+            scanf(" %99[^\n]", stringPrincipal);
+            getchar();
+        
+            printf("Digite a substring a ser buscada (max 99 caracteres): ");
+            scanf("%99[^\n]", substring);
+            getchar();
+        
+            if (contemSubstring(stringPrincipal, substring)) {
+                printf("\nA substring \"%s\" ESTA contida na string principal.\n", substring);
+            } else {
+                printf("\nA substring \"%s\" NAO esta contida na string principal.\n", substring);
+            }
+            break;
+        }
+        default: {
+            printf("Opcao invalida! Digite um numero entre 1 e 4.\n");
+        }
+    }
+
+    return 0;
+}
+
+
+void calcularFibonacci(int numero) {
+    unsigned long long fib[numero];
+    fib[0] = 0;
+    if (numero > 1) {
+        fib[1] = 1;
+    }
+
+    for (int i = 2; i < numero; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    printf("Os %d primeiros termos da sequencia de Fibonacci: \n", numero);
+    for (int i = 0; i < numero; i++) {
+        printf("%llu ", fib[i]);
+    }
+    printf("\n");
+}
+
+void calcularFatoriais(int limite, unsigned long long resultado[]) {
+    resultado[0] = 1;
+    for (int i = 2; i <= limite; i++) {
+        resultado[i - 1] = resultado[i - 2] * i;
+    }
+}
+
 int verificar_palindromo(const char *palavra) {
     int i, j;
     int tamanho = strlen(palavra);
@@ -77,64 +140,20 @@ int verificar_palindromo(const char *palavra) {
     return 1;
 }
 
-int main() {
-    printf("Voce escolheu a opcao de verificar Palindromo\n");
-    char palavra[101];
-    int eh_palindromo;
-
-    printf("Digite uma palavra (sem espaços, ate 100 letras): ");
-    scanf("%100s", palavra); 
-    getchar();
+int contemSubstring(const char *stringPrincipal, const char *substring) {
+    int lenPrincipal = strlen(stringPrincipal);
+    int lenSub = strlen(substring);
     
-    eh_palindromo = verificar_palindromo(palavra);
-    
-    if (eh_palindromo) {
-        printf("\"%s\" e um palindromo!\n", palavra);
-    } else {
-        printf("\"%s\" nao e um palindromo.\n", palavra);
-    }
-            break;
-        }
-        case 4: {
-            char stringPrincipal[100], substring[100];
-            int i, j, encontrou;
-        
-            printf("Digite a string principal (max 99 caracteres): ");
-            scanf(" %99[^\n]", stringPrincipal);
-            getchar();
-        
-            printf("Digite a substring a ser buscada (max 99 caracteres): ");
-            scanf("%99[^\n]", substring);
-            getchar();
-        
-            int lenPrincipal = strlen(stringPrincipal);
-            int lenSub = strlen(substring);
-        
-            encontrou = 0;
-        
-            for (i = 0; i <= lenPrincipal - lenSub; i++) {
-                for (j = 0; j < lenSub; j++) {
-                    if (tolower(stringPrincipal[i + j]) != tolower(substring[j])) {
-                        break;
-                    }
-                }
-                if (j == lenSub) {
-                    encontrou = 1;
-                    break;
-                }
+    for (int i = 0; i <= lenPrincipal - lenSub; i++) {
+        int j;
+        for (j = 0; j < lenSub; j++) {
+            if (tolower(stringPrincipal[i + j]) != tolower(substring[j])) {
+                break;
             }
-        
-            if (encontrou) {
-                printf("\nA substring \"%s\" ESTA contida na string principal.\n", substring);
-            } else {
-                printf("\nA substring \"%s\" NAO esta contida na string principal.\n", substring);
-            }
-            break;
         }
-        default: {
-            printf("Opcao invalida! Digite um numero entre 1 e 4.\n");
+        if (j == lenSub) {
+            return 1;
         }
     }
-
     return 0;
 }
